0b90d1323a5f6ae0b58574de7d12a49fa2a833c2
aryan-sharma3107 <as7854610@gmail.com>
2026-09-17 23:49:32 +0530

    Update Account request.java

--- Account request.java
+++ Account request.java
@@ -1,1 +1,23 @@
+package com.bankingportal.model;
 
+public class AccountRequest {
+
+    private double amount;
+    private String pin;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+}
