5df5878e4d0c6bce5470c8304445383935d6ba11
aryan-sharma3107 <as7854610@gmail.com>
2026-09-17 23:51:25 +0530

    Update Fund transfer request.java

--- Fund transfer request.java
+++ Fund transfer request.java
@@ -1,1 +1,32 @@
+package com.bankingportal.model;
 
+public class TransferRequest {
+
+    private double amount;
+    private String pin;
+    private String targetAccountNumber;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public String getTargetAccountNumber() {
+        return targetAccountNumber;
+    }
+
+    public void setTargetAccountNumber(String targetAccountNumber) {
+        this.targetAccountNumber = targetAccountNumber;
+    }
+}
